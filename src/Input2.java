ba59c98ba604b357733300e5c09943d8b672b9fa
100138808 <kindyl.hogan245@gmail.com>
2023-09-19 08:36:47 -0500

    Input 2

---
+++ src/Input2.java
@@ -0,0 +1,31 @@
+//NAME - Kindyl Hogan
+//DATE - 9/19/23
+//PURPOSE -
+
+import java.util.Scanner;
+
+public class Input2
+{
+    public static void main(String[] args)
+    {
+
+
+    Scanner keyboard = new Scanner(System.in);
+
+
+        String stringOne, stringTwo;
+
+
+        System.out.print("Enter your birthday :: ");
+        stringOne = keyboard.next();
+
+        System.out.print("Enter your name :: ");
+        stringTwo = keyboard.next();
+
+        System.out.println();
+
+
+        System.out.println("String One = " + stringOne );
+        System.out.println("String Two = " + stringTwo );
+         }
+    }
